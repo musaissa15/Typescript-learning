f3b332345edb881fd6b3a21d1bf97e8ccc2c8048
Musa Imad Issa <musaissa004@gmail.com>
2022-12-29 23:26:47 +0000

    intial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+function humanYearsCatYearsDogYears(humanYears) {
+    // Your code here!
+    let result = [];
+    console.log(result);
+    return [0, 0, 0];
+}
---
+++ index.ts
@@ -0,0 +1,6 @@
+ function humanYearsCatYearsDogYears(humanYears: number): [number, number, number] {
+  // Your code here!
+  let result: number[] = []
+  console.log(result)
+  return [0,0,0];
+}
