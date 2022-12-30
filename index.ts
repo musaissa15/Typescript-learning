26256d3c86d5fe121437cf3c53f8790145a2aa02
Musa Imad Issa <musaissa004@gmail.com>
2022-12-30 00:25:58 +0000

    Get rid of abortsignal error

--- index.ts
+++ index.ts
@@ -1,6 +1,28 @@
  function humanYearsCatYearsDogYears(humanYears: number): [number, number, number] {
-  // Your code here!
-  let result: number[] = []
-  console.log(result)
-  return [0,0,0];
+	// Your code here!
+	let result: number[] = [];
+	console.log(result);
+	return [0, 0, 0];
 }
+/** 
+ * !Kata Task
+*TODOS I have a cat and a dog.
+
+*TODOS I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+*TODOS Return their respective ages now as [humanYears,catYears,dogYears]
+
+* !NOTES:
+
+*TODOS humanYears >= 1
+*TODOS humanYears are whole numbers only
+* !Cat Years
+*TODOS 15 cat years for first year
+*TODOS +9 cat years for second year
+*TODOS +4 cat years for each year after that
+* !Dog Years
+*TODOS 15 dog years for first year
+*TODOS +9 dog years for second year
+*TODOS +5 dog years for each year after that
+ * 
+*/
